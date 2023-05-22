d0f7b6b653dd0f133564cef631e506a96cfe140f
Dan C <djc5627@gmail.com>
2023-05-21 23:21:09 -0400

    Fix missing scripts.

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -67,7 +67,6 @@ namespace Player
 
         protected virtual void OnPrimary(InputValue value)
         {
-            Debug.Log($"Interact: {value}");
             if (_isPlayerInputDisabled.Value) return;
             if (!value.isPressed) return;
             
